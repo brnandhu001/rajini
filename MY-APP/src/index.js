d5728edb46b3a69f3044ba1e770f34e5dab46651
brnandhu001 <85947143+brnandhu001@users.noreply.github.com>
2021-09-14 22:02:29 +0530

    Add files via upload

---
+++ MY-APP/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Login from './Log-in/Login';
+import Main from "./Home/Home"
+import Signup from "./Log-in/Sign"
+import Wallets from "./Wallet_pg/Wallet"
+
+
+ReactDOM.render(
+  <React.StrictMode>
+  {/* <Wallets/> */}
+    {/* <Login /> */}
+    <Main/>
+    {/* <Signup/> */}
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
