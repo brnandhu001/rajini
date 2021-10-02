78a65e173c503950a2c1ffd22080e1c3e15f7f3e
brnandhu001 <85947143+brnandhu001@users.noreply.github.com>
2021-10-02 16:33:36 +0530

    Add files via upload

--- MY-APP/src/Home/Home_modules/Main.jsx
+++ MY-APP/src/Home/Home_modules/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from '../Navbar_module/Navbar';
 import Home_top from './Home_top';
 import Top_selling from './Home_middle';
+import Check_out from './home_bottom/check_out';
+import Footer from './Footerr/Footer_main';
 
 
 export default function Homemain(){
@@ -9,6 +11,9 @@ export default function Homemain(){
     <Home_top/>   {/*main heading  and paragraph and button*/}
 
     <Top_selling/>    {/*topsellings to top sellers ----pls refer home_middle.jsx file*/}
+
+    <Check_out/>
+    <Footer/>
     
     
     </>
