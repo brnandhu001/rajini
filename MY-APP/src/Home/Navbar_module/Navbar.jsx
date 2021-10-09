54b05814e46054d6f06fa82ecdadff15954cdc6b
brnandhu001 <85947143+brnandhu001@users.noreply.github.com>
2021-10-09 22:02:05 +0530

    Add files via upload

--- MY-APP/src/Home/Navbar_module/Navbar.jsx
+++ MY-APP/src/Home/Navbar_module/Navbar.jsx
@@ -2,20 +2,28 @@ import React, { useState } from 'react';
 import { Link, BrowserRouter as Router, Route, Switch, } from "react-router-dom";
 
 
-// import  Homepg from '../../Home_modules/Home';
 
 
-// import  Login from "../../../Log-in/Login"
-// import Signup from '../../../Log-in/Sign';
-// import Wallets from "../../../Wallet_pg/Wallet"
-// import Home from '../../Home_modules/Home';
+import  Login from "../../Log-in/Login"
+import Signup from '../../Log-in/Sign';
+import Wallets from "../../Wallet_pg/Wallet"
+
 import com_logo from "../../Home/Home_modules/Im/com_logo.png"
 import "./navbar.css";
+import Homemain from '../Home_modules/Main';
+import About from '../../Pages/About/About';
+import Collections from "../../Pages/Collections/Collections"
+import Blog from "../../Pages/Blog/Blog";
+import How_it_works from "../../Pages/How_it_works/How_it_works";
+import Marketplace from '../../Pages/Marketplace/Marketplace';
+
+
 
 
 
 
 export default function Navbar() {
+     
 
   
     return (
@@ -25,7 +33,9 @@ export default function Navbar() {
                 <div className="nav">
             
                     <div>
-                        <img className="com-logo" src={com_logo} alt="Logo" />
+                    <ul>
+                       <li><Link to="/"><img className="com-logo" src={com_logo} alt="Logo" /></Link></li>
+                       </ul>
                         
                         
                     </div>
@@ -36,17 +46,17 @@ export default function Navbar() {
                         <ul  >
 
 
-                            <li className='li' >COLLECTIONS</li>
+                            <li className='li' ><Link to="/collections">COLLECTIONS</Link></li>
 
 
 
-                            <li className='li ' >MARKETPLACE</li>
+                            <li className='li ' ><Link to="/marketplace">MARKETPLACE</Link></li>
 
-                            <li className='li ' >HOW IT WORKS</li>
-                            <li className='li' >BLOG</li>
-                            <li className='li'>ABOUT</li>
+                            <li className='li ' ><Link to="/how it works">HOW IT WORKS</Link></li>
+                            <li className='li' ><Link to="/blog">BLOG</Link></li>
+                            <li className='li'><Link to="/about">ABOUT</Link></li>
                             {/* <li className='li'><i class='fas fa-wallet' id="wal-ic1" ></i></li> */}
-                            <li className='li'><i class='fas fa-wallet' id="wal-ic" ></i></li>
+                            <li className='li'><Link to="/wallet"><i class='fas fa-wallet' id="wal-ic" ></i></Link></li>
                             {/* <button className="mob">btn</button> */}
                             
                             
@@ -127,26 +137,26 @@ export default function Navbar() {
 
 
 
-            {/* <Switch> */}
+            <Switch>
 
-                {/* <Route exact path="/">
-            <Home/>
+                <Route exact path="/">
+            <Homemain></Homemain>
             </Route>
             
             <Route exact path="/about">
-            <About/>
+            <About></About>
             </Route>
             <Route exact path="/collections">
-            <Collections/>
+            <Collections></Collections>
             </Route>
             <Route exact path="/how it works">
             <How_it_works/>
             </Route>
-            <Route exact path="/allaboutnfts">
-            <All_about/>
+            <Route exact path="/blog">
+            <Blog/>
             </Route>
             <Route exact path="/marketplace">
-            <Market/>
+            <Marketplace></Marketplace>
             </Route>
             <Route exact path="/login">
             <Login/>
@@ -158,8 +168,8 @@ export default function Navbar() {
             <Route exact path="/wallet">
              <Wallets/>
         
-            </Route>  */}
-            {/* </Switch> */}
+            </Route> 
+            </Switch>
         </Router>
 
 
