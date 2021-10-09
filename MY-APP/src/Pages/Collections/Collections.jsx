4778516d491376068d6601fa839c262a11f50dd2
brnandhu001 <85947143+brnandhu001@users.noreply.github.com>
2021-10-09 21:58:13 +0530

    Add files via upload

--- MY-APP/src/Pages/Collections/Collections.jsx
+++ MY-APP/src/Pages/Collections/Collections.jsx
@@ -1,1 +1,12 @@
-qqq
+import React from 'react'
+
+function Collections() {
+    return (
+        <div>
+        <h1>Collections</h1>
+            
+        </div>
+    )
+}
+
+export default Collections
